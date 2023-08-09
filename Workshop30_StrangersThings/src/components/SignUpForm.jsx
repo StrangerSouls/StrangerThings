032ac722dbc5a4df88e5a799acc1041b3c4af18d
DebFullstack <debbieking1979@gmail.com>
2023-08-09 18:21:37 -0400

    completed basic functionality of signupform, needs welcome message still

--- Workshop30_StrangersThings/src/components/SignUpForm.jsx
+++ Workshop30_StrangersThings/src/components/SignUpForm.jsx
@@ -1,16 +1,22 @@
-import { useState, useEffect} from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function SignUpForm({ setToken }) {
-  const [userName, setUsername] = useState(""); 
-  const [password, setPassword] = useState(""); 
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); // New state for password confirmation
   const [error, setError] = useState(null);
+
   const COHORT_NAME = '2302-ACC-CT-WEB-PT-B';
   const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log(`You got this! $$`);
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
 
     const registerUser = async () => {
       try {
@@ -30,9 +36,9 @@ export default function SignUpForm({ setToken }) {
         const result = await response.json();
         
         console.log(result);
-		if (result && result.data && result.data.token) {
-			setToken(result.data.token);
-		}
+        if (result && result.data && result.data.token) {
+          setToken(result.data.token);
+        }
         return result;
       } catch (err) {
         console.error(err);
@@ -50,25 +56,38 @@ export default function SignUpForm({ setToken }) {
           Username:
         </label>
         <input
+          type="text"
           value={userName}
           onChange={(e) => setUsername(e.target.value)}
+          required
         />
         <label>
           Password:
         </label>
         <input
+          type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
+        <label>
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        {error && <p>{error}</p>}
         <button type="submit">
           Submit
         </button>
       </form>
     </div>
   )
-  
 }
 
 SignUpForm.propTypes = {
-	setToken: PropTypes.func.isRequired,
-  };
+  setToken: PropTypes.func.isRequired,
+};
