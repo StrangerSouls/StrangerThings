8f74c90f63d2d308797dac3fb283847f1b9b1edd
DebFullstack <debbieking1979@gmail.com>
2023-08-11 13:33:17 -0400

    included messages to and from user after log in on Home page

--- Workshop30_StrangersThings/src/components/Home.jsx
+++ Workshop30_StrangersThings/src/components/Home.jsx
@@ -1,42 +1,53 @@
 import { useEffect, useState } from 'react';
 import { BASE_URL } from '../utlities/constants';
+import { getTokenFromSessionStorage } from '../auth/sessionStorage';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const authToken = getTokenFromSessionStorage();
 
   useEffect(() => {
     async function fetchPosts() {
       try {
-        const response = await fetch(`${BASE_URL}/posts`);
+        const response = await fetch(`${BASE_URL}/posts`, {
+          headers: {
+            'Authorization': `Bearer ${authToken}`
+          }
+        });
         const data = await response.json();
-		console.log(data)
-        setPosts(data.data.posts); 
+        setPosts(data.data.posts);
       } catch (error) {
         console.log(`Error fetching all posts`);
       }
     }
 
     fetchPosts();
-  }, []);
+  }, [authToken]);
 
   return (
-    <div>
-   
-      
-    <div>
-      {posts.map((post) => (
-        <div key={post._id}>
-          <h2>{post.title}</h2>
-			<p>{post.description}</p>
-			<p>{post.price}</p>
-			<p>Location: {post.location}</p>
-			<p>Delivery Available? {post.WillDeliver}</p>
-			
-
-
-        </div>
-      ))}
-    </div>
+    <div>  
+      <div>
+        {posts.map((post) => (
+          <div key={post._id}>
+            <h2>{post.title}</h2>
+            <p>{post.description}</p>
+            <p>{post.price}</p>
+            <p>Location: {post.location}</p>
+            <p>Delivery Available? {post.willDeliver ? 'Yes' : 'No'}</p>
+            {authToken && (post.isAuthor || post.messages.length > 0) ? (
+              <div>
+                {/* Display messages for posts created by the logged-in user */}
+                {post.messages.map((message) => (
+                  <div key={message._id}>
+                    <p>Message from: {message.fromUser.username}</p>
+                    <p>Message: {message.content}</p>
+                  </div>
+                ))}
+              </div>
+            ) : null}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
