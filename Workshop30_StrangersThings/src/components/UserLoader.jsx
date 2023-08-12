56a628ca61ca50aa6c96cc83f61d10705bf2d242
DebFullstack <debbieking1979@gmail.com>
2023-08-12 18:54:42 -0400

    added css to userme and home

--- Workshop30_StrangersThings/src/components/UserLoader.jsx
+++ Workshop30_StrangersThings/src/components/UserLoader.jsx
@@ -4,6 +4,8 @@ import { getTokenFromSessionStorage } from '../auth/sessionStorage';
 import { Link } from "react-router-dom";
 import { handleDeletePost } from "../utilities/deletePost";
 
+import './UserLoader.css'
+
 function UserLoader() {
 	const [user, setUser] = useState(null);
 
@@ -35,36 +37,37 @@ function UserLoader() {
 	};
   
 	return (
-		<div>
+		<div className="user-loader-container">
 		{user ? (
 		<div>
-			<h2>Welcome, {user.username}</h2>
+			<h2 className="user-loader-title">Welcome, {user.username}</h2>
 			<h3>Your Posts</h3>
-			<div>
+			<div >
 				<ul>
 				{user.posts
 				.filter(post => post.author._id === user._id) // Filter posts by author's _id
 				.map(post => (
-					<li key={post._id}>{post.title}</li>
+					<li className="post" key={post._id}>{post.title}</li>
 				))}
 				</ul>
 				<ul>
 				{user.posts.map(post => (
-					<li key={post._id}>
-						{post.title}
-						<button onClick={() => handlePostDelete(post._id)}>Delete</button>
-						</li>
-					
-				))}
+  <li className="post" key={post._id}>
+    <div className="post-container">
+      <p className="post-title">{post.title}</p>
+      <button className="delete-button" onClick={() => handlePostDelete(post._id)}>Delete</button>
+    </div>
+  </li>
+))}
 				</ul>
 
 			</div>
 			<h3>Messages Received</h3>
-			<ul>
+			<ul className="message-container">
 			{user.messages
 				.filter(message => message.fromUser._id !== user._id) // Filter messages sent by others
 				.map(message => (
-				<div key={message._id}>
+				<div className="message" key={message._id}>
 					<p>From: {message.fromUser.username}</p>
 					<p>Content: {message.content}</p>
 					<p>Post: {message.post.title}</p>
@@ -72,11 +75,11 @@ function UserLoader() {
 				))}
 			</ul>
 			<h3>Messages You Sent</h3>
-			<ul>
+			<ul className="message-container">
 			{user.messages
             .filter(message => message.post && message.post.author._id !== user._id) // Filter messages sent by the user
             .map(message => (
-              <div key={message._id}>
+              <div className="message" key={message._id}>
                 <p>To User: {message.post.author ? message.post.author.username : 'Unknown User'}</p>
                 <p>Content: {message.content}</p>
                 <p>Post: {message.post ? message.post.title : 'Unknown Post'}</p>
